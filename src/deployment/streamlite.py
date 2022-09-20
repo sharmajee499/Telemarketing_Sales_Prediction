e9f823b17cdc7e682d17d75271fc1759c072ebec
sharmajee499 <sandeshsharma499@gmail.com>
2022-09-20 10:15:38 -0500

    first upload

---
+++ src/deployment/streamlite.py
@@ -0,0 +1,237 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+import lime.lime_tabular
+
+
+# ******************************************************************************
+# INPUT BUTTONS AND TEXTBOX
+# ******************************************************************************
+
+st.header("Telemarketing Success Prediction")
+
+st.sidebar.title("Please, enter the details of the respective customer")
+
+# Age Input
+input_age = st.sidebar.number_input("Customer's Age", min_value=18, max_value=140)
+
+# Job Input
+job_list = [
+    "housemaid",
+    "services",
+    "admin.",
+    "blue-collar",
+    "technician",
+    "retired",
+    "management",
+    "unemployed",
+    "self-employed",
+    "entrepreneur",
+    "student",
+]
+input_job = st.sidebar.selectbox("Customer's Job", options=job_list)
+
+# Maritial Status
+marital_list = ["married", "single", "divorced"]
+input_marital = st.sidebar.selectbox("Marital Status", options=marital_list)
+
+# Education Status
+education_list = [
+    "basic.4y",
+    "high.school",
+    "basic.6y",
+    "basic.9y",
+    "professional.course",
+    "university.degree",
+    "illiterate",
+]
+input_education = st.sidebar.selectbox("Education Level", options=education_list)
+
+# Housing Loan
+input_housing = st.sidebar.selectbox("Housing Loan", options=["yes", "no"])
+
+# Personal Loan
+input_personal_loan = st.sidebar.selectbox("Personal Loan", options=["yes", "no"])
+
+# Contact
+contact_list = ["telephone", "cellular"]
+input_contact = st.sidebar.selectbox("Customer Contacted With", options=contact_list)
+
+# Month
+month_list = ["may", "jun", "jul", "aug", "oct", "nov", "dec", "mar", "apr", "sep"]
+input_month = st.sidebar.selectbox("Month Contacted", month_list)
+
+# Day of Week
+day_list = ["mon", "tue", "wed", "thu", "fri"]
+input_day = st.sidebar.selectbox("Cantact Day", options=day_list)
+
+# Campaign
+input_campaign = st.sidebar.number_input(
+    "# of Contact Performed this campaign", min_value=0
+)
+
+# Contacted previous campaign
+input_previous_campaign = st.sidebar.number_input(
+    "# of Contacts On Previous Campaign", min_value=0
+)
+
+# Previous Outcome
+poutcome_list = ["nonexistent", "failure", "success"]
+input_poutcome = st.sidebar.selectbox("Previous Campaign Result", options=poutcome_list)
+
+# Employment Variation Rate
+input_emp_var_rate = st.sidebar.number_input(label="Employee Variation Rate")
+
+# Consumer Price Index
+input_cons_price_ind = st.sidebar.number_input(label="Consumer Price Index")
+
+# Consumer Confidence Index
+input_cons_confd_ind = st.sidebar.number_input("Consumer Confidence Index")
+
+# Eurobar 3-month Rate
+input_eurobar = st.sidebar.number_input("Eurobar (3 Months) Rate")
+
+# Number of Employees
+input_employees = st.sidebar.number_input(
+    "Quarterly Average of Total number of Employeed Citizen"
+)
+
+
+# ******************************************************************************
+# Formating the Input
+# ******************************************************************************
+feature_names = [
+    "age",
+    "job",
+    "marital",
+    "education",
+    "housing",
+    "loan",
+    "contact",
+    "month",
+    "day_of_week",
+    "campaign",
+    "previous",
+    "poutcome",
+    "emp.var.rate",
+    "cons.price.idx",
+    "cons.conf.idx",
+    "euribor3m",
+    "nr.employed",
+]
+
+# list of all provided input
+all_input = [
+    input_age,
+    input_job,
+    input_marital,
+    input_education,
+    input_housing,
+    input_personal_loan,
+    input_contact,
+    input_month,
+    input_day,
+    input_campaign,
+    input_previous_campaign,
+    input_poutcome,
+    input_emp_var_rate,
+    input_cons_price_ind,
+    input_cons_confd_ind,
+    input_eurobar,
+    input_employees,
+]
+
+# Loading the train data
+X = np.load(
+    r"C:\Users\Sande\OneDrive - MNSCU\Telemarketing_Success_Paper\Code+Analytics\Telemarketing_Success_Paper\src\deployment\X.npy",
+    allow_pickle=True,
+)
+
+# Copy of X
+X_lime = np.copy(X)
+
+# Transformation
+cat_features_ind = [1, 2, 3, 4, 5, 6, 7, 8, 11]
+
+# Categorical Name
+categorical_names = {}
+
+for ind in cat_features_ind:
+    le = LabelEncoder()
+    X[:, ind] = le.fit_transform((X[:, ind]))
+    categorical_names[ind] = le.classes_
+    all_input[ind] = le.transform([all_input[ind]])
+
+input_arr = []
+for elements in all_input:
+    try:
+        input_arr.append(int(elements[0]))
+    except:
+        input_arr.append(int(elements))
+
+encoder = ColumnTransformer(
+    [("ohe", OneHotEncoder(), cat_features_ind)], remainder="passthrough"
+)
+
+data = encoder.fit(X)
+
+# ******************************************************************************
+# Prediction
+# ******************************************************************************
+
+# Open the pickled model
+with open(
+    r"C:\Users\Sande\OneDrive - MNSCU\Telemarketing_Success_Paper\Code+Analytics\Telemarketing_Success_Paper\src\deployment\lgbm_final_model",
+    "rb",
+) as f:
+    lgbm_model = pickle.load(f)
+
+# transforming the 'input_arr'
+input_encoded = data.transform(np.array(input_arr).reshape(1, -1))
+
+# Prediction
+predict = lgbm_model.predict_proba(encoder.transform(input_encoded))
+predict2 = lgbm_model.predict(encoder.transform(input_encoded))
+
+# ******************************************************************************
+# Explaination
+# ******************************************************************************
+
+# Predict function for the LIME
+predict_fn = lambda x: lgbm_model.predict_proba(encoder.transform(x))
+
+# LIME Explainer
+explainer = lime.lime_tabular.LimeTabularExplainer(
+    X,
+    feature_names=feature_names,
+    class_names=["No", "Yes"],
+    discretize_continuous=True,
+    categorical_features=cat_features_ind,
+    categorical_names=categorical_names,
+    kernel_width=3,
+)
+
+# Instance Explaination
+np.random.seed(112233)
+exp = explainer.explain_instance(np.array(all_input), predict_fn, num_features=7)
+
+
+col1, col2, col3 = st.columns(3)
+prob_zero = exp.predict_proba[0] * 100
+prob_one = exp.predict_proba[1] * 100
+col1.metric("Buying Probability %", f"{prob_one:.2f}")
+col2.metric("Rejecting Probability %", f"{prob_zero:.2f}")
+col3.metric(f"Threshold %", value="19")
+
+st.caption(
+    """Since our threshold is 19%, if the buying probability is greater than 19%, 
+    that means customer will buy the deposite."""
+)
+
+
+# Plot
+st.pyplot(exp.as_pyplot_figure())
---
+++ src/pre_process_helper_function/pre_processing.py
@@ -0,0 +1,271 @@
+# ******************************************************************************
+# IMPORTING THE LIBRARIES
+# ******************************************************************************
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# ******************************************************************************
+# PRE-PROCESSING CLASS
+# ******************************************************************************
+
+
+class pre_processing:
+    def __init__(
+        self,
+        path="C:/Users/Sande/OneDrive - MNSCU/Telemarketing_Success_Paper/Code+Analytics/Telemarketing_Success_Paper/data/raw/bank-additional-full.csv",
+        standard=True,
+    ):
+        """
+        Please provide the path as string or it will use the default path
+        """
+        # It will read the data as soon as the class is constructed
+        self._path = path
+        self._df = pd.read_csv(self._path, sep=";", na_values=["unknown"])
+        self._edu_map = {
+            "illiterate": 1,
+            "basic.4y": 2,
+            "basic.6y": 3,
+            "basic.9y": 4,
+            "high.school": 5,
+            "university.degree": 6,
+            "professional.course": 7,
+        }
+        self._one_hot_var = [
+            "job",
+            "marital",
+            "housing",
+            "loan",
+            "contact",
+            "month",
+            "day_of_week",
+            "poutcome",
+            "y",
+        ]
+        self._num_var = [
+            "age",
+            "campaign",
+            "previous",
+            "emp.var.rate",
+            "cons.price.idx",
+            "cons.conf.idx",
+            "euribor3m",
+            "nr.employed",
+        ]
+        self._standard = standard
+
+    # function to get the default path
+    def get_path(self):
+        return self._path
+
+    # Getting the info of the df
+    def data_info(self):
+        return self._df.info()
+
+    # Get the descriptive stat of the data_frame
+    def data_desc_stat(self):
+        return self._df.describe(include="all")
+
+    # Getting the Null values info
+    def data_null_info(self):
+        return self._df.isna().sum()
+
+    # cleans the pre_processing data
+    def clean_df(self):
+
+        # Remove the variables**************************************************
+        var_rem = ["pdays", "default", "duration"]
+        df = self._df
+        df.drop(columns=var_rem, axis=1, inplace=True)
+
+        # Null Values Removal***************************************************
+        df.interpolate(method="pad", limit=2, inplace=True)
+        df.dropna(inplace=True)
+
+        # Ordinal Encoder*******************************************************
+        edu_encode = df["education"].map(self._edu_map)
+        edu_encode_df = pd.DataFrame(edu_encode, columns=["education"])
+        edu_encode_df.reset_index(inplace=True, drop=True)
+
+        # One Hot Encoding******************************************************
+        oh_encode_df = pd.get_dummies(df[self._one_hot_var], drop_first=False)
+        oh_encode_df.drop(["y_no"], axis=1, inplace=True)
+
+        # drop the 'y_no' variable as it is redundent to 'y_yes'
+        # oh_encode_df.drop(columns=["y_no"], inplace=True)
+
+        # reseting the index of the oh_encode_df
+        oh_encode_df.reset_index(inplace=True, drop=True)
+
+        # Standardization of Numeric Variable***********************************
+
+        # Subseting the df with num var
+        df_num = self._df[self._num_var]
+        df_num.reset_index(inplace=True, drop=True)
+
+        # combining 'edu_encode_df' with numeric var for normalization
+        df_merged = pd.concat([df_num, edu_encode_df], axis=1)
+
+        # output the non-standard data-frame if needed
+        if self._standard == False:
+            df_final = pd.concat([df_merged, oh_encode_df], axis=1)
+            return df_final
+
+        # extracting column name for later purpose
+        col_name = df_merged.columns
+
+        std_num = StandardScaler().fit_transform(df_merged)
+        std_df = pd.DataFrame(std_num, columns=col_name)
+
+        # Combining the standardize data with one-hot
+        df_final = pd.concat([std_df, oh_encode_df], axis=1)
+
+        return df_final
+
+    def no_encoded_df(self):
+    
+        # Remove the variables**************************************************
+        var_rem = ["pdays", "default", "duration"]
+        df = self._df
+        df.drop(columns=var_rem, axis=1, inplace=True)
+
+        # Null Values Removal***************************************************
+        df.interpolate(method="pad", limit=2, inplace=True)
+        df.dropna(inplace=True)
+        return df
+
+
+
+# ******************************************************************************
+# HELPER FUNCTION - Transform the Standardized data
+# ******************************************************************************
+
+# Tranforming the standardized data to the original format
+def reverse_transform_original(var_name, value):
+    """
+    This is transform back the standardized values to the original form.
+    raw_df_path: path to the raw file
+    var_name: variable name
+    value: value that needed to be transformed back to original
+
+    Returns: double number/integer
+    """
+    raw_df_path = "C:/Users/Sande/OneDrive - MNSCU/Telemarketing_Success_Paper/Code+Analytics/Telemarketing_Success_Paper/data/raw/bank-additional-full.csv"
+    raw_df = pd.read_csv(raw_df_path, sep=";")
+
+    if var_name == "education":
+        # Ordinal Encoder: 'education'
+        edu_map = {
+            "illiterate": 1,
+            "basic.4y": 2,
+            "basic.6y": 3,
+            "basic.9y": 4,
+            "high.school": 5,
+            "university.degree": 6,
+            "professional.course": 7,
+        }
+
+        edu_encode = raw_df["education"].map(edu_map)
+        edu_encode_df = pd.DataFrame(edu_encode, columns=["education"])
+
+        rev_transform = round(
+            np.std(edu_encode_df["education"]) * value
+            + np.mean(edu_encode_df["education"]),
+            2,
+        )
+        return rev_transform
+
+    rev_transform = round(
+        np.std(raw_df[var_name]) * value + np.mean(raw_df[var_name]), 2
+    )
+
+    return rev_transform
+
+
+# ******************************************************************************
+# HELPER FUNCTION - LIME TRANSFORMATION -- Extra
+# ******************************************************************************
+"""At first the 'LIME' is implemented on the standardized data but to get the 
+correct value that is transformed back to the original. Basically we will change
+the original lime plot from API to our original data plot. We are kust changing
+the plot index"""
+
+
+def lime_transform(exp):
+    """Input the lime explaination object for a specific instance
+    Output: The new dataframe with index and its repesctive value to make new
+    lime plot."""
+
+    # Extract the exp object result as list
+    exp_lst = [i[0] for i in exp.as_list()]
+
+    # Split the combined string into list of strings
+    exp_lst_split = [i.split() for i in exp_lst]
+
+    # Convert the string that represent float to float type
+
+    # Initialize the empty list
+    exp_lst_float = []
+
+    for items in exp_lst_split:
+        # Another empty list to store calculation
+        lst_sub = []
+        for i in items:
+            # if the sub-string can be converted into float then try else...
+            try:
+                float(i)
+                lst_sub.append(float(i))
+            except ValueError:
+                lst_sub.append(i)
+
+        exp_lst_float.append(lst_sub)
+
+    # variables with the standardized data
+    trsf_var = [
+        "age",
+        "campaign",
+        "previous",
+        "emp.var.rate",
+        "cons.price.idx",
+        "cons.conf.idx",
+        "euribor3m",
+        "nr.employed",
+    ]
+
+    trsf_lst = []
+
+    # global variable for storing info in later loop
+    var1 = ""
+
+    # This loop only change the variables that are standardized keeping all things same
+    for items in exp_lst_float:
+        for i in items:
+            if i in trsf_var:
+                var1 = i
+        if var1 in items:
+            trsf_sub_lst = []
+            for i in items:
+                if type(i) == float:
+                    trsf_sub_lst.append(reverse_transform_original(var1, i))
+                else:
+                    trsf_sub_lst.append(i)
+            trsf_lst.append(trsf_sub_lst)
+        else:
+            trsf_lst.append(items)
+
+    # Converting the list into string
+    trsf_lst = [[" ".join([str(i) for i in trsf_lst[j]])] for j in range(len(trsf_lst))]
+
+    # converting to numpy array
+    trsf_lst = np.array(trsf_lst)
+
+    # Flatten the array
+    trsf_lst = trsf_lst.flatten()
+
+    # Extract the 'value' component of the lime output
+    val = np.array([i[1] for i in exp.as_list()])
+
+    # Combining the transformed name and value to a data-frame
+    trsf_df = pd.DataFrame(val, index=trsf_lst, columns=["prob"])
+
+    return trsf_df
